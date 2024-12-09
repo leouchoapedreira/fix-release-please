576c8e6a98835bf3ef1552f68a416f338ca11277
GitHub Action <action@github.com>
2024-12-09 20:47:54 +0000

    Update version to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 _ = setuptools.setup(
     name="custom_transformers",
-    version="0.0.0",
+    version="1.1.0",
     author="Leonardo Uchôa Pedreira",
     author_email="leonardo.pedreira@samplemed.com.br",
     description="Custom Transformers",
